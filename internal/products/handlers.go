b6f3107c6f5bf88cc5a059fa9bf6fe4fbba21e86
Qasim-Hassan <qasimhassan1947@gmail.com>
2026-09-15 13:16:11 +0500

    implementing services/handlers

--- internal/products/handlers.go
+++ internal/products/handlers.go
@@ -1,5 +1,18 @@
 package products
 
+import "net/http"
+
 type handler struct {
 	service Service
+}
+
+func NewHandler(s Service) *handler {
+	return &handler{
+		service: s,
+	}
+}
+
+func (h *handler) ListProducts(w http.ResponseWriter, r *http.Request) {
+	// 1. CALL the serice → return products
+	// 2. Return JSON in an HTTP response
 }
